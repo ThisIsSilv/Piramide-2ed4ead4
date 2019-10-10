f741c55eb30589b018bd691c48bc00d9e52a776a
Silvano Bakacak <2119823@talnet.nl>
2019-10-10 09:30:46 +0200

    Uitwerking van de exercise

--- piramide.php
+++ piramide.php
@@ -1,9 +1,12 @@
 <?php
+echo "Hoegroet moet de driehoenk zijn";
 $sterretjes = readline();
 
-for ($pyr1 = 1; $pyr1 <= $sterretjes; $pyr1++) {
-    for($pyr2 = 1; $pyr2<=$pyr1; $pyr2++) {
-    echo  "*";
+for ($pyr1 = 1; $pyr1 <= $sterretjes; $pyr1++)
+{
+    for($pyr2 = 1; $pyr2<=$pyr1; $pyr2++)
+    {
+        echo  "*";
     }
     echo "\n";
 }
